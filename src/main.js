0b84ce75d917ca9b3914f3d771ce89c24d614c03
stevesmart1113 <stevesmart1113@gmail.com>
2021-01-20 15:54:19 +0000

    Added some changes to the programs API

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ Vue.use(VueAxios, axios)
 Vue.use(vueStarRate)
 
 window.axios = axios
-axios.defaults.baseURL = "http://157.230.209.212:8083/"
+axios.defaults.baseURL = "http://d.bluespaceafrica.com:8083/"
 
 new Vue({
   el: '#app',
